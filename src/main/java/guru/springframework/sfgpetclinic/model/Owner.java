795d4ffc81a12a33b2c0819835403da91a5aec12
yared2 <yared@gmail.com>
2020-07-16 01:21:25 -0500

    adding POJOs. closes #2

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * @author yared
+ */
+public class Owner extends Person {
+}
---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,7 @@
+package guru.springframework.sfgpetclinic.model;
+
+/**
+ * @author yared
+ */
+public class Vet extends Person {
+}
